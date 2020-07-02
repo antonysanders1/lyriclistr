8defadcd76df106f35c38dc115c9bad4a738b4ea
antony_sanders <antony.devon92@gmail.com>
2020-07-02 13:40:48 -0400

    1. Created Songs controller w/methods. 2. Added song routes

---
+++ api-backend/app/controllers/songs_controller.rb
@@ -0,0 +1,39 @@
+class SongsController < ApplicationController
+
+    def index
+        @songs = Song.all
+
+        render json: @songs, status: 200
+    end
+
+    def show
+       @song = Song.find(params:id)
+       
+       render json: @note, status: 200
+    end
+
+    def create
+        @song = Song.create(song_params)
+        if @song.create
+            render json: @song, status: 200
+        else
+            render json: {message: @song.errors.messages}
+        end
+    end
+
+    def update
+        @song = Song.find(params[:id])
+        if @song.update
+            render json: @song, status: 200
+        else
+            render json: {message: @song.errors.messages}
+        end
+    end
+
+    private 
+
+    def song_params
+        params.require(:song).permit(:title, :genre, :lyrics)
+    end
+
+end
