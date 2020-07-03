e20aa3441997ed14d6887ddd03c9f46d3569505e
antony_sanders <antony.devon92@gmail.com>
2020-07-03 03:51:02 -0400

    1. Made minor changes to create new song btn function

--- frontend/show.js
+++ frontend/show.js
@@ -3,13 +3,15 @@ document.getElementById("new-song-btn").addEventListener("click", function(e) {
     document.getElementById("welcome").style.display = "none";
     document.getElementById("new-form-container").style.display = "inline";
     document.getElementById("cancel-btn").style.display = "inline"
+    document.getElementById("or").style.display = "none"
     document.getElementById("new-song-btn").style.display = "none"
 })
 
 document.getElementById("cancel-btn").addEventListener("click", function(e) {
     e.preventDefault();
-    document.getElementById("welcome").style.display = "inline";
+    document.getElementById("welcome").style.display = "block";
     document.getElementById("new-form-container").style.display = "none";
     document.getElementById("new-song-btn").style.display = "inline"
+    document.getElementById("or").style.display = "inline"
     document.getElementById("cancel-btn").style.display = "none"
 })
