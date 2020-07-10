a7ee4679ca02ce20fc08c0b97890f90c8bb13906
antony_sanders <antony.devon92@gmail.com>
2020-07-10 03:35:42 -0400

    Added code that replaces the song display with new data

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    getSong();
+    getSongTitle();
+    songSelect();
     newSong();
     // getComments();
     //createComment();
@@ -7,48 +8,97 @@ document.addEventListener("DOMContentLoaded", function() {
 
 const service = new ApiAdapter;
 
-function getSong() {
-    service.getSongs("songs")
+function getSongTitle() {
+    service.getSongTitles("songs")
         .then(data => {
             data.forEach(index => {
                 const song = new Song(index)
+                song.showSongTitle()
             })
         })
 }
 
-function newSong() {
-    let form = document.getElementById("new-song");
-    let song = document.getElementById("song");
+function songSelect() {
+    let selector = document.getElementById('song-selector')
+        //let main = document.getElementById('main')
+    let song = document.getElementById("song-container");
+    console.log(song)
     let songTitle = document.createElement('h2');
     let songArtist = document.createElement('h4');
     let songLyrics = document.createElement('p');
-    form.addEventListener("submit", function(e) {
+    selector.addEventListener('change', function(e) {
         e.preventDefault()
+        document.getElementById("main-container").style.display = "none";
+        let songId = e.target.value
+        service.getSong(songId)
+            .then(data => {
+                let selectedSong = new Song(data)
+                    //let song = document.createElement('div')
+                songTitle.innerHTML = selectedSong.title
+                songTitle.setAttribute('id', 'song-title')
+                songArtist.innerHTML = selectedSong.artist
+                songArtist.setAttribute('id', 'song-artist')
+                songLyrics.innerText = selectedSong.lyrics
+                songLyrics.setAttribute('id', 'song-lyrics')
+                song.appendChild(songTitle)
+                song.appendChild(songArtist)
+                song.appendChild(songLyrics)
+                    //main.appendChild(song)
+                song.style.display = "inline";
+            })
+
+    })
+
+}
+
+function newSong() {
+    let main = document.getElementById('main')
+    let form = document.getElementById("new-song");
+    let song = document.getElementById('song-container');
+    form.addEventListener("submit", function(e) {
+        e.preventDefault();
         const title = e.target[0].value
         const artist = e.target[1].value
         const lyrics = e.target[2].value
         service.fetchCreateSong(title, artist, lyrics).then(data => { let newSong = new Song(data) })
         document.getElementById("main-container").style.display = "none";
-        songTitle.innerHTML = title;
-        songArtist.innerHTML = artist;
-        songLyrics.innerText = lyrics;
-        songLyrics.setAttribute('id', 'song-lyrics')
-        song.appendChild(songTitle)
-        song.appendChild(songArtist)
-        song.appendChild(songLyrics)
+
+        if (!!document.getElementById('song-title')) {
+            let previousSongTitle = document.querySelector('h2');
+            let previousSongArtist = document.querySelector('h4');
+            let previousSongLyrics = document.getElementById('song-lyrics');
+            song.removeChild(previousSongTitle)
+            song.removeChild(previousSongArtist)
+            song.removeChild(previousSongLyrics)
+            let songTitle = document.createElement('h2')
+            songTitle
+            let songArtist = document.createElement('h4')
+            let songLyrics = document.createElement('p')
+            songTitle.innerHTML = title;
+            songTitle.setAttribute('id', 'song-title')
+            songArtist.innerHTML = artist;
+            songArtist.setAttribute('id', 'song-artist')
+            songLyrics.innerText = lyrics;
+            songLyrics.setAttribute('id', 'song-lyrics')
+            song.append(songTitle, songArtist, songLyrics)
+        } else {
+            let songTitle = document.createElement('h2')
+            let songArtist = document.createElement('h4')
+            let songLyrics = document.createElement('p')
+            songTitle.innerHTML = title;
+            songTitle.setAttribute('id', 'song-title')
+            songArtist.innerHTML = artist;
+            songArtist.setAttribute('id', 'song-artist')
+            songLyrics.innerText = lyrics;
+            songLyrics.setAttribute('id', 'song-lyrics')
+            song.append(songTitle, songArtist, songLyrics)
+        }
         song.style.display = "inline";
+
+
     })
 
 }
-
-
-
-
-
-
-
-
-
 
 
 function getComment() {
