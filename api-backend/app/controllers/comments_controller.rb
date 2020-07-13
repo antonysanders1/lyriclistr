87a694b7ae22a35b2e92f53aff421f035a33e169
antony_sanders <antony.devon92@gmail.com>
2020-07-13 01:52:36 -0400

    Fixed gramatical errors and broken syntax in comments_controller

--- api-backend/app/controllers/comments_controller.rb
+++ api-backend/app/controllers/comments_controller.rb
@@ -2,18 +2,18 @@ class CommentsController < ApplicationController
     def index
         @comments = Comment.all
 
-        render json: @comment, status: 200
+        render json: @comments, status: 200
     end
 
     def show
-       @comment = Comment.find(params:id)
+       @comment = Comment.find(params[:id])
        
        render json: @comment, status: 200
     end
 
     def create
         @comment = Comment.create(comment_params)
-        if @song.create
+        if @song.valid?
             render json: @comment, status: 200
         else
             render json: {message: @comment.errors.messages}
