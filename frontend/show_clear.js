d24cbd826c5a468880baa45159d9c08e6f40dafc
antony_sanders <antony.devon92@gmail.com>
2020-07-17 04:17:39 -0400

    Cleaned up syntax. Deleted unused code

--- frontend/show_clear.js
+++ frontend/show_clear.js
@@ -1,20 +1,17 @@
  document.getElementById("new-song-btn").addEventListener("click", function(e) {
      e.preventDefault();
      document.getElementById("welcome").style.display = "none";
+     document.getElementById('comments-container').style.display = "none";
      document.getElementById("main-container").style.display = "inline";
      document.getElementById("new-form-container").style.display = "inline";
      document.getElementById('song-container').style.display = "none"
-     document.getElementById('song-selector').value = "default"
+     document.getElementById('song-selector').style.display = "none"
      let input1 = document.getElementById('input1');
      let input2 = document.getElementById('input2');
      let input3 = document.getElementById('input3');
      input1.value = ''
      input2.value = ''
      input3.value = ''
-         //     document.getElementById("cancel-btn").style.display = "inline"
-         //     document.getElementById("or").style.display = "none"
-         //     document.getElementById("new-song-btn").style.display = "none"
-
  })
 
  document.getElementById("cancel-btn").addEventListener("click", function(e) {
